f9b11ab95db9788c6e95c4b1435ea4570d769b0d
Sky <sky_of_izalith@gmail.com>
2023-05-27 11:59:46 +0100

    capture point mechanics implemented

---
+++ Assets/Scripts/Level/CaptureZone.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class CaptureZone : MonoBehaviour
+{
+    public float captureTime = 5f; // The time required to capture the zone.
+    private bool isPlayerInside = false;
+
+    public static event Action OnCapture; // Event to be triggered when zone is captured
+
+    private IEnumerator CaptureProcess() // Coroutine to handle capture process
+    {
+        yield return new WaitForSeconds(captureTime);
+        if (isPlayerInside) {
+            Capture();
+        }
+    }
+
+    private void Capture()
+    {
+        OnCapture?.Invoke(); // Trigger the OnCapture event
+        Destroy(gameObject); // Destroy the current zone after it is captured
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player") {
+            isPlayerInside = true;
+            StartCoroutine(CaptureProcess());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player") {
+            isPlayerInside = false;
+            StopCoroutine(CaptureProcess());
+        }
+    }
+}
--- Assets/Scripts/Other/GraphicsSettings.cs
+++ Assets/Scripts/Other/GraphicsSettings.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class GraphicsSettings : MonoBehaviour
 {
+    public bool limitFps = false;
+    public bool vSync = false;
     public int fpsCap = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Application.targetFrameRate = fpsCap;
-        // QualitySettings.vSyncCount = 1;
+        
+        if (limitFps) {
+            Application.targetFrameRate = fpsCap;
+        }
+
+        if (vSync) {
+            QualitySettings.vSyncCount = 1;
+        }
     }
 }
