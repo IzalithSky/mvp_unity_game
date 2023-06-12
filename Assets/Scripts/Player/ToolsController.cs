5ace9c816bb66a30d5d40b8fda32ff803cea0069
Sky <sky_of_izalith@gmail.com>
2023-06-12 01:27:00 +0100

    persistant ammo

--- Assets/Scripts/Player/ToolsController.cs
+++ Assets/Scripts/Player/ToolsController.cs
@@ -11,10 +11,31 @@ public class ToolsController : MonoBehaviour
     public Transform firePoint;
     public List<GameObject> toolExamples;
 
+    private List<Tool> tools = new List<Tool>();
+
+    void Start() {
+        // Instantiate all tools in a disabled state
+        foreach (var toolExample in toolExamples) {
+            GameObject toolObject = Instantiate(toolExample, toolHolder.transform);
+            toolObject.SetActive(false);
+            
+            Tool tool = toolObject.GetComponent<Tool>();
+            tool.lookPoint = lookPoint;
+            tool.firePoint = firePoint;
+            
+            DamageSource damageSource = toolObject.GetComponent<DamageSource>();
+            if (null != damageSource) {
+                damageSource.owner = owner;
+            }
+
+            tools.Add(tool);
+        }
+    }
+
     void Update() {
         for(int i = 0; i < inputListener.toolListLength; i++) {
             if (inputListener.GetIsTool(i)) {
-                if (toolExamples.Count > i) {
+                if (tools.Count > i) {
                     SetTool(i);
                 }
             }
@@ -22,20 +43,13 @@ public class ToolsController : MonoBehaviour
     }
 
     void SetTool(int toolIndex) {
+        // Disable current tool
         if (null != toolHolder.currentTool) {
-            Destroy(toolHolder.currentTool.gameObject);
+            toolHolder.currentTool.gameObject.SetActive(false);
         }
 
-        GameObject toolObject = Instantiate(toolExamples[toolIndex], toolHolder.transform);
-        Tool tool = toolObject.GetComponent<Tool>();
-
-        tool.lookPoint = lookPoint;
-        tool.firePoint = firePoint;
-        toolHolder.currentTool = tool;
-
-        DamageSource damageSource = toolObject.GetComponent<DamageSource>();
-        if (null != damageSource) {
-            damageSource.owner = owner;
-        }
+        // Set and enable new tool
+        toolHolder.currentTool = tools[toolIndex];
+        toolHolder.currentTool.gameObject.SetActive(true);
     }
 }
