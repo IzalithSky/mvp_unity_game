0ddb38cf96a6f507ba43373c57fcdf2ca088ba3f
Sky <sky_of_izalith@gmail.com>
2023-06-21 09:34:12 +0100

    reset fix

--- Assets/Scripts/Level/SceneLoader.cs
+++ Assets/Scripts/Level/SceneLoader.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
-{
+public class SceneLoader : MonoBehaviour {
     public void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;
     }
 
     public void QuitGame() {
         Application.Quit();
+    }
+
+    public void ResetScene() {
+        Scene scene = SceneManager.GetActiveScene(); 
+        LoadScene(scene.name);
     }
 }
