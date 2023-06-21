9b33f76ea4061addc7b63912e18c20be89655dd3
Sky <sky_of_izalith@gmail.com>
2023-06-21 09:39:50 +0100

    adjustments

--- Assets/Scripts/Common/Respawnable.cs
+++ Assets/Scripts/Common/Respawnable.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Respawnable : Damageable
 {
     public Transform respawn;
+    public SceneLoader sceneLoader;
 
     public override void Die() {
-        hp = maxHp;
-        transform.position = respawn.position;
-        transform.rotation = respawn.rotation;
+        if (null == sceneLoader) {
+            hp = maxHp;
+            transform.position = respawn.position;
+            transform.rotation = respawn.rotation;
+        } else {
+            sceneLoader.ResetScene();
+        }
     }
 }
