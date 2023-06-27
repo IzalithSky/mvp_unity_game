ffdefc6f3f24eac9ae34475d7c3e50e39c6c2921
Sky <sky_of_izalith@gmail.com>
2023-06-27 15:29:40 +0100

    tool direction

--- Assets/Scripts/Player/ToolHolder.cs
+++ Assets/Scripts/Player/ToolHolder.cs
@@ -6,11 +6,27 @@ public class ToolHolder : MonoBehaviour
 {
     public InputListener inputListener;
     public Tool currentTool;
+    public Transform lookPoint;
+    public float defaultAimDistance = 1000f;
 
     // Update is called once per frame
     void Update()
     {
         if (inputListener.GetIsWalking()) {
+            Ray ray = new Ray(lookPoint.position, lookPoint.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                float distance = Vector3.Distance(lookPoint.position, hit.point);
+                transform.LookAt(hit.point);
+            }
+            else
+            {
+                // No object is under the camera's gaze. You can either do nothing or
+                // look at a point at a default distance in front of the camera.
+                transform.LookAt(lookPoint.position + lookPoint.forward * defaultAimDistance);
+            }
+
             if (inputListener.GetIsFiring() && null != currentTool) {
                 currentTool.Fire();
             }
--- Assets/Scripts/Player/ToolSway.cs
+++ Assets/Scripts/Player/ToolSway.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ToolSway : MonoBehaviour {
-    public float amount = 20f;
-    public float maxamount = 20f;
     public float smooth = 3f;
     public float rightOffset = .3f;
     public float walkSwayIntensity = 0.0025f;
@@ -22,20 +20,11 @@ public class ToolSway : MonoBehaviour {
             float inV = Input.GetAxis("Vertical");
             float inH = Input.GetAxis("Horizontal");
 
-            float fx = -inV * amount + Input.GetAxis("Mouse Y") * amount;
-            float fy = -inH * amount - Input.GetAxis("Mouse X") * amount;
-            float fz = -inH * amount;
             float fr = -inH * rightOffset;
             float ff = -inV * rightOffset;
             
-            fx = Mathf.Clamp(fx, -maxamount * .3f, maxamount *.3f);
-            fy = Mathf.Clamp(fy, -maxamount, maxamount);
-            fz = Mathf.Clamp(fz, -maxamount, maxamount);
             fr = Mathf.Clamp(fr, -rightOffset, rightOffset);
             ff = Mathf.Clamp(ff, -rightOffset * .3f, rightOffset * .3f);
-
-            Quaternion finalRotation = Quaternion.Euler(defaultRotation.x + fx, defaultRotation.y + fy, defaultRotation.z + fz);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, finalRotation, Time.deltaTime * smooth);
 
             Vector3 finalPosition = new Vector3(defaultPosition.x + fr, defaultPosition.y, defaultPosition.z + ff);
             transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition, Time.deltaTime * rightOffset * smooth);
