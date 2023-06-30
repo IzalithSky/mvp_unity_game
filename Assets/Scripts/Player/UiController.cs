1641a0197de00fb90da359a13971fff03b05f650
Sky <IzalithSky@users.noreply.github.com>
2023-07-01 00:36:58 +0100

    adjustments

--- Assets/Scripts/Player/UiController.cs
+++ Assets/Scripts/Player/UiController.cs
@@ -8,11 +8,11 @@ public class UiController : MonoBehaviour
 {
     public TMP_Text hpText;
     public Damageable damagable;
-    
+
     public TMP_Text toolNameText;
     public TMP_Text ammoCountText;
     public ToolHolder toolHolder;
-    
+
     public TMP_Text fpsText;
     public TMP_Text velocityText;
     public Rigidbody rb;
@@ -20,68 +20,60 @@ public class UiController : MonoBehaviour
     public TMP_Text zoneCapturedText;
     public CapturePointSpawner capturePointSpawner;
 
-    public Image topImage;
-    public Image bottomImage;
-    public Image leftImage;
-    public Image rightImage;
-
-    public Image topLeftImage;
-    public Image topRightImage;
-    public Image bottomLeftImage;
-    public Image bottomRightImage;
+    public Image[] directionIndicators; // 0: Top, 1: Bottom, 2: Left, 3: Right, 4: TopLeft, 5: TopRight, 6: BottomLeft, 7: BottomRight
 
     public float fadeDuration = 1f;
-
 
     float dt = 0.0f;
     float fps = 0.0f;
     float updateRate = 4.0f;  // 4 updates per sec.
     int frameCount = 0;
 
+    // Track each coroutine
+    private Dictionary<Image, Coroutine> imageFadingCoroutines = new Dictionary<Image, Coroutine>();
+
 
     void Start()
     {
-        hpText.text = "0";
-        
-        toolNameText.text = "";
-        ammoCountText.text = "";
-
-        fpsText.text = "0";
-        velocityText.text = "0";
-
-        zoneCapturedText.text = "";
+        UpdateText(hpText, "0");
+        UpdateText(toolNameText, "");
+        UpdateText(ammoCountText, "");
+        UpdateText(fpsText, "0");
+        UpdateText(velocityText, "0");
+        UpdateText(zoneCapturedText, "");
     }
 
     void Update()
     {
-        hpText.text = damagable.GetHp().ToString();
+        UpdateText(hpText, damagable.GetHp().ToString());
 
-        if (null != toolHolder.currentTool) {
-            toolNameText.text = toolHolder.currentTool.toolName;
-            if (toolHolder.currentTool.usesAmmo) {
-                ammoCountText.text = toolHolder.currentTool.ammoCount.ToString();
-            } else {
-                ammoCountText.text = "∞";
-            }
+        if (null != toolHolder.currentTool)
+        {
+            UpdateText(toolNameText, toolHolder.currentTool.toolName);
+            UpdateText(ammoCountText, toolHolder.currentTool.usesAmmo ? toolHolder.currentTool.ammoCount.ToString() : "∞");
         }
-        
-        UpdateFps();
-        fpsText.text = fps.ToString("F2");
-        
-        velocityText.text = new Vector3(rb.velocity.x, 0f, rb.velocity.z).magnitude.ToString("F2");
 
-        if (null != capturePointSpawner) {
-            zoneCapturedText.text = 
-                capturePointSpawner.zonesCapturedTotal.ToString() + 
-                "/" + 
-                capturePointSpawner.zonesCapturedMax.ToString();
+        UpdateFps();
+        UpdateText(fpsText, fps.ToString("F2"));
+        UpdateText(velocityText, new Vector3(rb.velocity.x, 0f, rb.velocity.z).magnitude.ToString("F2"));
+
+        if (null != capturePointSpawner)
+        {
+            UpdateText(zoneCapturedText, $"{capturePointSpawner.zonesCapturedTotal}/{capturePointSpawner.zonesCapturedMax}");
         }
     }
 
-    void UpdateFps() {
+    void UpdateText(TMP_Text textComponent, string value)
+    {
+        textComponent.text = value;
+    }
+
+    void UpdateFps()
+    {
         frameCount++;
         dt += Time.deltaTime;
-        if (dt > 1.0f / updateRate) {
+        if (dt > 1.0f / updateRate)
+        {
             fps = frameCount / dt;
             frameCount = 0;
             dt -= 1.0f / updateRate;
@@ -90,73 +82,53 @@ public class UiController : MonoBehaviour
 
     public void ShowDamageDirection(Vector3 damageSourcePosition)
     {
-        // Get player's position and forward/right direction
-        Vector3 playerPos = Camera.main.transform.position;
-        Vector3 playerForward = Camera.main.transform.forward;
-        Vector3 playerRight = Camera.main.transform.right;
+        Vector3 direction = (damageSourcePosition - Camera.main.transform.position).normalized;
+        float forwardDot = Vector3.Dot(direction, Camera.main.transform.forward);
+        float rightDot = Vector3.Dot(direction, Camera.main.transform.right);
 
-        // Get the direction from the player to the damage source
-        Vector3 direction = (damageSourcePosition - playerPos).normalized;
+        Debug.Log("forward " + forwardDot + " right " + rightDot);
 
-        // Calculate the dot products
-        float forwardDot = Vector3.Dot(direction, playerForward);
-        float rightDot = Vector3.Dot(direction, playerRight);
+        Dictionary<string, Image> directions = new Dictionary<string, Image> {
+            {"Top", directionIndicators[0]}, {"Bottom", directionIndicators[1]}, {"Left", directionIndicators[2]}, {"Right", directionIndicators[3]},
+            {"TopLeft", directionIndicators[4]}, {"TopRight", directionIndicators[5]}, {"BottomLeft", directionIndicators[6]}, {"BottomRight", directionIndicators[7]}
+        };
 
-        // Determine which direction the damage came from
-        if (forwardDot > 0.7f)
-        {
-            // Damage came from the front (top)
-            LightIndicator(topImage);
-        }
-        else if (forwardDot < -0.7f)
-        {
-            // Damage came from the back (bottom)
-            LightIndicator(bottomImage);
-        } 
-        else if (rightDot > 0.7f)
-        {
-            // Damage came from the right
-            LightIndicator(rightImage);
-        }
-        else if (rightDot < -0.7f)
-        {
-            // Damage came from the left
-            LightIndicator(leftImage);
-        }
-        // Check for corner directions
-        else if(Mathf.Abs(forwardDot) <= 0.7f && Mathf.Abs(rightDot) <= 0.7f) {
-            if(forwardDot > 0 && rightDot > 0) {
-                // Front-Right (Top-Right)
-                LightIndicator(topRightImage);
-            } else if(forwardDot > 0 && rightDot < 0) {
-                // Front-Left (Top-Left)
-                LightIndicator(topLeftImage);
-            } else if(forwardDot < 0 && rightDot > 0) {
-                // Back-Right (Bottom-Right)
-                LightIndicator(bottomRightImage);
-            } else if(forwardDot < 0 && rightDot < 0) {
-                // Back-Left (Bottom-Left)
-                LightIndicator(bottomLeftImage);
-            }
+        if (forwardDot > 0.6f) LightIndicator(directions["Top"]);
+        else if (forwardDot < -0.6f) LightIndicator(directions["Bottom"]);
+        else if (rightDot > 0.6f) LightIndicator(directions["Right"]);
+        else if (rightDot < -0.6f) LightIndicator(directions["Left"]);
+        else if (Mathf.Abs(forwardDot) <= 0.6f && Mathf.Abs(rightDot) <= 0.6f) {
+            if (forwardDot > 0 && rightDot > 0) LightIndicator(directions["TopRight"]);
+            else if (forwardDot > 0 && rightDot < 0) LightIndicator(directions["TopLeft"]);
+            else if (forwardDot < 0 && rightDot > 0) LightIndicator(directions["BottomRight"]);
+            else if (forwardDot < 0 && rightDot < 0) LightIndicator(directions["BottomLeft"]);
         }
     }
 
-
-    void LightIndicator(Image image) {
+    void LightIndicator(Image image)
+    {
         image.color = new Color(image.color.r, image.color.g, image.color.b, 0.5f);
-        StartCoroutine(FadeImageInAndOut(image, fadeDuration));
+        // Stop the previous coroutine for the same image if it is running
+        if(imageFadingCoroutines.ContainsKey(image) && imageFadingCoroutines[image] != null)
+        {
+            StopCoroutine(imageFadingCoroutines[image]);
+            imageFadingCoroutines[image] = null;
+        }
+        // Start a new coroutine
+        imageFadingCoroutines[image] = StartCoroutine(FadeImageInAndOut(image, fadeDuration));
     }
 
     IEnumerator FadeImageInAndOut(Image image, float duration)
-    {        
-        // Fade image out
+    {
         float elapsedTime = 0f;
-        while(elapsedTime < duration)
+        while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
             float alpha = Mathf.Lerp(0.5f, 0f, elapsedTime / duration);
             image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
             yield return null;
         }
+        // Cleanup
+        imageFadingCoroutines[image] = null;
     }
 }
