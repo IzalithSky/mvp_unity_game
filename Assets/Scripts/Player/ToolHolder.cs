308c5dc5325d5a9161956c14aa4fc289f360651e
Sky <IzalithSky@users.noreply.github.com>
2023-07-02 17:37:49 +0100

    tool jitter fix

--- Assets/Scripts/Player/ToolHolder.cs
+++ Assets/Scripts/Player/ToolHolder.cs
@@ -9,12 +9,20 @@ public class ToolHolder : MonoBehaviour
     public Transform lookPoint;
     public float defaultAimDistance = 1000f;
 
+    LayerMask mask ;
+
+    void Start() {
+        string[] transparentLayers = new string[] { "Tools", "Projectiles", "Trigger", "Smoke" };
+        mask = ~LayerMask.GetMask(transparentLayers); 
+    }
+
     // Update is called once per frame
     void Update()
     {
+
         Ray ray = new Ray(lookPoint.position, lookPoint.forward);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, mask))
         {
             float distance = Vector3.Distance(lookPoint.position, hit.point);
             transform.LookAt(hit.point);
