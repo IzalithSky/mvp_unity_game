b27cf30c8557acaa41312690457546175cbc2f0c
Sky <IzalithSky@users.noreply.github.com>
2023-07-02 16:50:12 +0100

    adjustments

--- Assets/Shaders/ProbeLauncher.cs
+++ Assets/Shaders/ProbeLauncher.cs
@@ -63,7 +63,6 @@ public class ProbeLauncher : MonoBehaviour
 
     void Update()
     {
-        // Get all keys from the dictionary
         List<string> keys = new List<string>(ringObjects.Keys);
 
         // For each pair of spheres
@@ -71,63 +70,88 @@ public class ProbeLauncher : MonoBehaviour
         {
             for (int j = i + 1; j < spheres.Count; j++)
             {
-                string key = i.ToString() + "_" + j.ToString();
-                keys.Remove(key);
-
-                float radius1 = spheres[i].transform.localScale.x / 2f;
-                float radius2 = spheres[j].transform.localScale.x / 2f;
-
-                Vector3 center1 = spheres[i].transform.position;
-                Vector3 center2 = spheres[j].transform.position;
-
-                Vector3 centerDiff = center2 - center1;
-                float distance = centerDiff.magnitude;
-
-                // Check if the spheres are intersecting
-                if (distance < radius1 + radius2 && distance > Mathf.Abs(radius1 - radius2))
-                {
-                    if (!ringObjects.ContainsKey(key))
-                    {
-                        // Create a new ring
-                        GameObject ringObject = new GameObject(ringNamePrefix + key);
-                        LineRenderer newRing = ringObject.AddComponent<LineRenderer>();
-                        newRing.loop = true;
-                        newRing.positionCount = ringSegments;
-                        newRing.startWidth = ringWidth;
-                        newRing.endWidth = ringWidth;
-                        newRing.material = ringMaterial;
-                        newRing.gameObject.layer = LayerMaskToLayer(ringLayerMask); // Assign the desired layer to the ring object
-
-                        ringObjects.Add(key, ringObject);
-                    }
-
-                    // Calculate the center and radius of the intersection circle
-                    Vector3 circleCenter = center1 + ((radius1 * radius1 - radius2 * radius2 + distance * distance) / (2 * distance)) * (centerDiff / distance);
-                    float circleRadius = Mathf.Sqrt(radius1 * radius1 - (circleCenter - center1).magnitude * (circleCenter - center1).magnitude);
-
-                    // Calculate the points around the intersection circle and assign them to the LineRenderer
-                    LineRenderer currentRing = ringObjects[key].GetComponent<LineRenderer>();
-                    Vector3 forward = (centerDiff / distance).normalized;
-                    Vector3 right = Vector3.Cross(forward, Vector3.up).normalized;
-                    Vector3 up = Vector3.Cross(right, forward).normalized;
-
-                    for (int k = 0; k < currentRing.positionCount; k++)
-                    {
-                        float angle = k * 2 * Mathf.PI / currentRing.positionCount;
-                        Vector3 direction = (Mathf.Cos(angle) * right + Mathf.Sin(angle) * up).normalized;
-                        currentRing.SetPosition(k, circleCenter + direction * circleRadius);
-                    }
-                }
-                else if (ringObjects.ContainsKey(key))
-                {
-                    // If the spheres are no longer intersecting, destroy the corresponding ring
-                    Destroy(ringObjects[key]);
-                    ringObjects.Remove(key);
-                }
+                ProcessSpherePair(i, j, keys);
             }
         }
 
         // Destroy the rings of the pairs of spheres that no longer exist
+        DestroyRemainingRings(keys);
+    }
+
+    private void ProcessSpherePair(int i, int j, List<string> keys)
+    {
+        string key = i.ToString() + "_" + j.ToString();
+        keys.Remove(key);
+
+        float radius1 = spheres[i].transform.localScale.x / 2f;
+        float radius2 = spheres[j].transform.localScale.x / 2f;
+
+        Vector3 center1 = spheres[i].transform.position;
+        Vector3 center2 = spheres[j].transform.position;
+
+        Vector3 centerDiff = center2 - center1;
+        float distance = centerDiff.magnitude;
+
+        // Check if the spheres are intersecting
+        if (distance < radius1 + radius2 && distance > Mathf.Abs(radius1 - radius2))
+        {
+            CreateOrAdjustRing(i, j, key, radius1, radius2, center1, center2, centerDiff, distance);
+        }
+        else if (ringObjects.ContainsKey(key))
+        {
+            // If the spheres are no longer intersecting, destroy the corresponding ring
+            Destroy(ringObjects[key]);
+            ringObjects.Remove(key);
+        }
+    }
+
+    private void CreateOrAdjustRing(int i, int j, string key, float radius1, float radius2, Vector3 center1, Vector3 center2, Vector3 centerDiff, float distance)
+    {
+        if (!ringObjects.ContainsKey(key))
+        {
+            ringObjects.Add(key, CreateNewRing(i, j, key));
+        }
+
+        AdjustRing(i, j, key, radius1, radius2, center1, center2, centerDiff, distance);
+    }
+
+    private GameObject CreateNewRing(int i, int j, string key)
+    {
+        // Create a new ring
+        GameObject ringObject = new GameObject(ringNamePrefix + key);
+        LineRenderer newRing = ringObject.AddComponent<LineRenderer>();
+        newRing.loop = true;
+        newRing.positionCount = ringSegments;
+        newRing.startWidth = ringWidth;
+        newRing.endWidth = ringWidth;
+        newRing.material = ringMaterial;
+        newRing.gameObject.layer = LayerMaskToLayer(ringLayerMask); // Assign the desired layer to the ring object
+
+        return ringObject;
+    }
+
+    private void AdjustRing(int i, int j, string key, float radius1, float radius2, Vector3 center1, Vector3 center2, Vector3 centerDiff, float distance)
+    {
+        // Calculate the center and radius of the intersection circle
+        Vector3 circleCenter = center1 + ((radius1 * radius1 - radius2 * radius2 + distance * distance) / (2 * distance)) * (centerDiff / distance);
+        float circleRadius = Mathf.Sqrt(radius1 * radius1 - (circleCenter - center1).magnitude * (circleCenter - center1).magnitude);
+
+        // Calculate the points around the intersection circle and assign them to the LineRenderer
+        LineRenderer currentRing = ringObjects[key].GetComponent<LineRenderer>();
+        Vector3 forward = (centerDiff / distance).normalized;
+        Vector3 right = Vector3.Cross(forward, Vector3.up).normalized;
+        Vector3 up = Vector3.Cross(right, forward).normalized;
+
+        for (int k = 0; k < currentRing.positionCount; k++)
+        {
+            float angle = k * 2 * Mathf.PI / currentRing.positionCount;
+            Vector3 direction = (Mathf.Cos(angle) * right + Mathf.Sin(angle) * up).normalized;
+            currentRing.SetPosition(k, circleCenter + direction * circleRadius);
+        }
+    }
+
+    private void DestroyRemainingRings(List<string> keys)
+    {
         foreach (string key in keys)
         {
             Destroy(ringObjects[key]);
