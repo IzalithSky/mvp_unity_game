0f263d45cff80adc98bea160ae84c11b73ef3d10
Sky <IzalithSky@users.noreply.github.com>
2023-07-03 19:01:52 +0100

    capgun

--- Assets/Scripts/Level/CaptureZone.cs
+++ Assets/Scripts/Level/CaptureZone.cs
@@ -1,41 +1,68 @@
 using UnityEngine;
-using System;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class CaptureZone : MonoBehaviour
 {
     public float captureTime = 5f; // The time required to capture the zone.
-    private bool isPlayerInside = false;
+    public List<string> capturingTags; // List of tags that can capture the zone.
+
+    private float currentCaptureTime = 0f;
+    private bool isCapturing = false;
+    private Coroutine captureCoroutine;
 
     public static event Action OnCapture; // Event to be triggered when zone is captured
 
-    private IEnumerator CaptureProcess() // Coroutine to handle capture process
+    public float CapturePercentage
     {
-        yield return new WaitForSeconds(captureTime);
-        if (isPlayerInside) {
-            Capture();
-        }
+        get { return currentCaptureTime / captureTime; }
     }
 
-    private void Capture()
+    public float cp = 0f;
+
+
+    void Update() {
+        cp = CapturePercentage;
+    }
+
+
+    private IEnumerator CaptureProcess() // Coroutine to handle capture process
     {
+        while (currentCaptureTime < captureTime)
+        {
+            yield return new WaitForEndOfFrame();
+            if (isCapturing)
+            {
+                currentCaptureTime += Time.deltaTime;
+            }
+        }
         OnCapture?.Invoke(); // Trigger the OnCapture event
         Destroy(gameObject); // Destroy the current zone after it is captured
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player") {
-            isPlayerInside = true;
-            StartCoroutine(CaptureProcess());
+        if (capturingTags.Contains(other.gameObject.tag))
+        {
+            isCapturing = true;
+            if (captureCoroutine == null)
+            {
+                captureCoroutine = StartCoroutine(CaptureProcess());
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.tag == "Player") {
-            isPlayerInside = false;
-            StopCoroutine(CaptureProcess());
+        if (capturingTags.Contains(other.gameObject.tag))
+        {
+            isCapturing = false;
+            if (captureCoroutine != null)
+            {
+                StopCoroutine(captureCoroutine);
+                captureCoroutine = null;
+            }
         }
     }
 }
